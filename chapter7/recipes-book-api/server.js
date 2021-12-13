0a65d38994ef1616ba2d969be4750e923421dd11
Lamis <lchebbi@vermeg.com>
2021-12-13 11:33:57 +0100

    feat(): Chapter 7 source code

---
+++ chapter7/recipes-book-api/server.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+server.use(jsonServer.defaults());
+const recipes = JSON.parse(fs.readFileSync('./db-json/recipes.json', 'UTF-8'));
+
+
+server.get('/api/recipes', (req, res) => {
+  res.status(200).json(recipes)
+})
+
+server.listen(3001, () => {
+  console.log('Run Auth API Server')
+})
+
